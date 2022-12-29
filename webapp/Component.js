327957074b61bd3522adef9559de17be3adf2274
dev-jens <baetenjens07@gmail.com>
2022-12-29 15:36:16 +0100

    initial

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("be.edu.ap.baetenjens.zsd040materialapp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
